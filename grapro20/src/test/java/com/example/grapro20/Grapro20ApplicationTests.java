b2e1527354749d3c95cc4d27febe1413044d7dab
161250187 <161250187@smail.nju.edu.cn>
2020-04-12 16:23:55 +0800

    add

---
+++ grapro20/src/test/java/com/example/grapro20/Grapro20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grapro20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grapro20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
